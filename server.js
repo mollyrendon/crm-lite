85a2c25612fd145994c252d6a4c348a2f31b2a8d
jbryanmcgrath <90098243+jbryanmcgrath@users.noreply.github.com>
2022-01-26 16:23:23 -0500

    idk

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ const helpers = require('./utils/helpers');
 const hbs = exphbs.create({ helpers });
 
 app.engine('handlebars', hbs.engine);
-//app.set('view engine', 'handlebars');
+app.set('view engine', 'handlebars');
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
